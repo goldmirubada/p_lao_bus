f4abdc1df0e23b7dfea664b12c45550161e9dd92
unknown <goldmiru@gmail.com>
2025-12-08 01:15:46 +0900

    feat: init hybrid app setup (Capacitor & PWA)

---
+++ android/app/src/main/java/com/goldmirubada/laobus/MainActivity.java
@@ -0,0 +1,5 @@
+package com.goldmirubada.laobus;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.goldmirubada.laobus',
+  appName: 'Lao Bus',
+  webDir: 'out'
+};
+
+export default config;
